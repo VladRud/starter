2d69c9ceadf561481b6710d5713925a22ef3896a
SegiiGoncharov <web4travel@gmail.com>
2018-05-10 00:59:08 +0300

    Refactor

--- client/app/admin/admin.module.js
+++ client/app/admin/admin.module.js
@@ -3,8 +3,6 @@
 import angular from 'angular';
 import authModule from './../../components/auth/auth.module';
 import uiRouter from '@uirouter/angularjs';
-// Fixme: set back new toggle, current  one does't support commonJS export
-// import uiToggle from 'angular-bootstrap-toggle';
 import AdminController from "./admin.controller";
 import UserDetailsController from "./user.details.controller";
 import AdminRouter from "./admin.router";
